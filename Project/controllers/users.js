9fdcb300c76b08b7cb3ff762566771e3eded0b50
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-15 23:45:26 +0530

    put generated names in USERS

--- Project/controllers/users.js
+++ Project/controllers/users.js
@@ -1,5 +1,8 @@
 const { Users } = require('./db/model')
-
+const { getRandomUserName } = require('./utils/username')
 async function createAnonUser(){
-
+    const user = await Users.create({
+        username : getRandomUserName()
+    })
+    return user
 }
