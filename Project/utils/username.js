d81709f357d88552bae157f74e2c817bc2837bb8
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-15 23:42:44 +0530

    create function for generating random names'

--- Project/utils/username.js
+++ Project/utils/username.js
@@ -1,7 +1,18 @@
 const ADJECTIVES = [
-    'boundless', 'passionate', 'invinsible', 'sleepy', 'dangerous', 'eggy', 'chickky', 'creative'
+    'boundless', 'passionate', 'invinsible', 'sleepy', 'dangerous', 'eggy', 'chickky', 'fleshy'
 ]
 
 const FOODS = [
-    'pizza', 'burger', 'pasta', 'fries', 'hotdog', 'macroni', 'sandwich', 'Dhokhla'
+    'pizza', 'burger', 'pasta', 'fries', 'hotdog', 'macroni', 'sandwich', 'Shushi'
 ]
+
+function getRandomUserName() {
+    let adj = ADJECTIVES[Math.floor(Math.random()*8)]
+    let food = FOODS[Math.floor(Math.random()*8)]
+    return `${adj}+${food}`
+
+}
+
+module.exports={
+    getRandomUserName
+}
