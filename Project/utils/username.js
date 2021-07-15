b5f579476301926711f5a06d0134650d1676dfac
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-15 23:36:22 +0530

    Food names for users

--- Project/utils/username.js
+++ Project/utils/username.js
@@ -1,3 +1,7 @@
 const ADJECTIVES = [
     'boundless', 'passionate', 'invinsible', 'sleepy', 'dangerous', 'eggy', 'chickky', 'creative'
 ]
+
+const FOODS = [
+    'pizza', 'burger', 'pasta', 'fries', 'hotdog', 'macroni', 'sandwich', 'Dhokhla'
+]
