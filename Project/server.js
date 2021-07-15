f5d52a983ffc528340501311a6d841350631274a
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-15 22:49:07 +0530

    add comments

--- Project/server.js
+++ Project/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const { db } = require('./db/models')
+//we can also write models = req('./db/models) the db  = models.db for imprting only one object or we can obtain one object like {db}
 
 app.listen(8833, ()=>{
     console.log("server is started on http://localhost:8833")
