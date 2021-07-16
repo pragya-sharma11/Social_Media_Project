a213bb3f38a4278e3e63beedf844a80347ad6ddb
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-16 14:49:46 +0530

    import modules from users file of controller module

--- Project/routes/users/index.js
+++ Project/routes/users/index.js
@@ -1,5 +1,5 @@
 const route =require('express').Router()
-
+const { createAnonUser, getUserById, getUserByUsername} = require('../../controllers/users')
 route.get('/', (req,res)=>{
     res.send('//TODO : All users.')
 })
