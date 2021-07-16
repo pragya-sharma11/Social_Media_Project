68d6c22864da952d30ba57c9dfc48c9c5a6ba79d
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-16 15:02:57 +0530

    if user exists then send data else error 404

--- Project/routes/users/index.js
+++ Project/routes/users/index.js
@@ -1,13 +1,19 @@
 const route =require('express').Router()
 const { createAnonUser, getUserById, getUserByUsername} = require('../../controllers/users')
-route.get('/', (req,res)=>{
+route.get('/', async (req,res)=>{
     let user ;
     if(isNaN(parseInt(req.params.id))){
         user = await getUserByUsername(req.params.id)
     }else{
         user = await getUserById(req.params.id)
     }
-    res.send('//TODO : All users.')
+   if(user) {
+       res.status(200).send(user)
+   } else {
+        res.status(404).send({
+            error: 'no such user id or username exists'
+        })
+   }
 })
 
 module.exports ={
