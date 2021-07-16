17bc5c3f29d1effbc784b8f9b34e4cca7738741b
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-16 14:59:10 +0530

    make functionality inside user route if id or username is given , we can easily identify user

--- Project/routes/users/index.js
+++ Project/routes/users/index.js
@@ -1,6 +1,12 @@
 const route =require('express').Router()
 const { createAnonUser, getUserById, getUserByUsername} = require('../../controllers/users')
 route.get('/', (req,res)=>{
+    let user ;
+    if(isNaN(parseInt(req.params.id))){
+        user = await getUserByUsername(req.params.id)
+    }else{
+        user = await getUserById(req.params.id)
+    }
     res.send('//TODO : All users.')
 })
 
