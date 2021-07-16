34d33f63e8b25fcbfb8218a9e3f315d3f81b0ecf
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-16 14:19:24 +0530

    in server make a path for each APIs

--- Project/server.js
+++ Project/server.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const { db } = require('./db/model')
 //we can also write models = req('./db/models) the db  = models.db for imprting only one object or we can obtain one object like {db}
+const {usersRoute } = require('./routes/users')
+const {postsRoute } = require('./routes/posts')
+const { urlencoded } = require('express')
+app.use(express.json)
+app.use(urlencoded({extended:true}))
+app.use('/api/users', usersRoute)
+app.use('/api/posts', postsRoute)
+app.use('/', express.static(__dirname+'/public'))
+
 
 db.sync({alter:true})     //its  a promising function so its used to synchronise with db
 .then(()=>{
