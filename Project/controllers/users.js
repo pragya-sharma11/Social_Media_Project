573dcf8a89c32048f090269dd4d52bf5f33de195
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-16 14:46:40 +0530

    create an async function getUserById for identifying users by their id

--- Project/controllers/users.js
+++ Project/controllers/users.js
@@ -1,10 +1,15 @@
 const { Users } = require('../db/model')
+const users = require('../routes/users')
 const { getRandomUserName } = require('../utils/username')
 async function createAnonUser(){
     const user = await Users.create({
         username : getRandomUserName()
     })
     return user
+}
+
+async function getUserById(){
+    return await users.findOne({id})
 }
 
 module.exports={
