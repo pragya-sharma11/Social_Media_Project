51b4946931538d2166e356edd98033827260f7b8
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-16 15:44:41 +0530

    use json and  urlencoded

--- Project/server.js
+++ Project/server.js
@@ -4,7 +4,8 @@ const { db } = require('./db/model')
 //we can also write models = req('./db/models) the db  = models.db for imprting only one object or we can obtain one object like {db}
 const { usersRoute }= require('./routes/users')
 const { postsRoute } = require('./routes/posts')
-
+app.use(express.json)
+app.use(express.urlencoded({extended:true}))
 app.use('/api/users', usersRoute)
 app.use('/api/posts', postsRoute)
 app.use('/', express.static(__dirname+'/public'))
