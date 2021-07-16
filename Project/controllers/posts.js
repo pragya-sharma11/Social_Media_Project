26f1d4921bf91bd2e219e6a388ab2a9ab490a12a
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-16 13:17:08 +0530

    creating function for test code

--- Project/controllers/posts.js
+++ Project/controllers/posts.js
@@ -4,6 +4,7 @@ async function createNewPost(userId, title, body){
     const post = await Posts.create({
         title,body,userId
     })
+    return post
 }
 
 /**
@@ -13,3 +14,17 @@ async function createNewPost(userId, title, body){
  async function showAllPosts(query){
 
 }
+
+/* Test code */
+
+async function task(){
+    console.log(await createNewPost(
+        1,'this is a simple post','Body of post goes here'
+    ))
+
+    console.log(await createNewPost(
+        2,'this is a simple post','Body of post goes here'
+    ))
+}
+
+task()
