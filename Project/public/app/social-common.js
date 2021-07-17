c674df89bce20ec25b7da63bc82729f86868ca25
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-17 19:58:30 +0530

    add a user and then made a call back function for username at nav bar

--- Project/public/app/social-common.js
+++ Project/public/app/social-common.js
@@ -1,7 +1,7 @@
 $(()=>{
-    $('#navbar').load('./components/navbar.html')
+    $('#navbar').load('./components/navbar.html',loginIfNeeded) //callback function is available at load().
     $('#footer').load('./components/footer.html')
-    loginIfNeeded()
+    
 
 })
 function loginIfNeeded(){
@@ -12,9 +12,14 @@ function loginIfNeeded(){
             if(user) {
                 console.log("Logged in as " + user.username)
                 window.localStorage.user = JSON.stringify(user)
+                console.log('nav-username')  //it is possible that before loading nav-bar and footer, our username get activated so we need to make our code top to bottom but not at one time, but line by line and one after another.
+                //so we will try some kind of call back function in jquery.
+                $('#nav-username').text(user.username)
             }
         })
     } else {
         console.log('resuming session as '+ currentUser.username)
+        console.log('nav-username')
+        $('#nav-username').text(currentUser.username)
     }
 }
