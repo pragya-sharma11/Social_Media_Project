9d611775ace96088ba941150f906580450b7deff
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-17 19:06:10 +0530

    post users api

--- Project/public/app/social-common.js
+++ Project/public/app/social-common.js
@@ -8,6 +8,8 @@ function loginIfNeeded(){
     let currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null;
     console.log(currentUser)
     if(!currentUser){
-        $.post('/api/users')
+        $.post('/api/users', {}, (user)=>{
+            window.localStorage.user = JSON.stringify(user)
+        })
     }
 }
