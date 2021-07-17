3fb58aa6ae4a709e3a846d8949cac6240f4a22c9
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-18 00:48:09 +0530

    some fixes

--- Project/public/app/social-common.js
+++ Project/public/app/social-common.js
@@ -1,12 +1,9 @@
-
-
 $(()=>{
-    
     $('#navbar').load('./components/navbar.html',loginIfNeeded) //callback function is available at load().
     //first loading of component done then only loginIfNeeded will execute.
     $('#footer').load('./components/footer.html')
     $('#content').load('./components/all-articles.html',)
-    loadPosts()
+  })
 function loginIfNeeded(){
   let currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null;
   if (!currentUser) {
@@ -25,23 +22,3 @@ function loginIfNeeded(){
   }
 }
 
-function loadPosts(){
-  $.get('/api/posts',(posts)=>{
-    for(let p of posts){
-      //console.log(p);---for testing purpose.
-      $('#posts-container').append(
-        $(`<div class="card" style="width: 18rem;">
-                <div class="card-body">
-                  <h5 class="card-title">${p.title}</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
-                  <p class="card-text">${p.body}</p>
-                  <a href="#" class="card-link">read more</a>
-                  <a href="#" class="card-link">like</a>
-                </div>
-              </div>
-              `)
-            )
-    }
-  })
-}
-})
