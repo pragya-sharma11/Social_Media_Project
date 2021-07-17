ecdc02eb61a4c68d04326c235a252b50c2749985
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-17 19:04:16 +0530

    create a function for login

--- Project/public/app/social-common.js
+++ Project/public/app/social-common.js
@@ -1,4 +1,13 @@
 $(()=>{
     $('#navbar').load('./components/navbar.html')
     $('#footer').load('./components/footer.html')
+    loginIfNeeded()
+
 })
+function loginIfNeeded(){
+    let currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null;
+    console.log(currentUser)
+    if(!currentUser){
+        $.post('/api/users')
+    }
+}
