1e2fc87cd3cc8dfbcf439958030a532d6785970b
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-18 00:36:54 +0530

    create function for showing the posts

--- Project/public/app/social-common.js
+++ Project/public/app/social-common.js
@@ -1,8 +1,12 @@
+
+
 $(()=>{
+    
     $('#navbar').load('./components/navbar.html',loginIfNeeded) //callback function is available at load().
     //first loading of component done then only loginIfNeeded will execute.
     $('#footer').load('./components/footer.html')
-    $('#content').load('./components/all-articles.html')
+    $('#content').load('./components/all-articles.html',)
+    loadPosts()
 function loginIfNeeded(){
   let currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null;
   if (!currentUser) {
@@ -20,25 +24,24 @@ function loginIfNeeded(){
     $('#nav-username').text(currentUser.username)
   }
 }
-/*
-    let currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null;
-    console.log(currentUser)
-    if(!currentUser){
-        $.post('/api/users', {}, (user)=>{
-            if(user) {
-                console.log("Logged in as " + user.username)
-                window.localStorage.user = JSON.stringify(user)
-                console.log('nav-username')  //it is possible that before loading nav-bar and footer, our username get activated so we need to make our code top to bottom but not at one time, but line by line and one after another.
-                //so we will try some kind of call back function in jquery.
-                $('#nav-username').text(user.username)
-            }
-        })
-    } else {
-        console.log('resuming session as '+ currentUser.username)
-        console.log('nav-username')
-        $('#nav-username').text("currentUser.username")
-        console.log($('#nav-username').val(currentUser.username))
+
+function loadPosts(){
+  $.get('/api/posts',(posts)=>{
+    for(let p of posts){
+      //console.log(p);---for testing purpose.
+      $('#posts-container').append(
+        $(`<div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">${p.title}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
+                  <p class="card-text">${p.body}</p>
+                  <a href="#" class="card-link">read more</a>
+                  <a href="#" class="card-link">like</a>
+                </div>
+              </div>
+              `)
+            )
     }
+  })
 }
-*/
 })
