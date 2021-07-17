4ba3793534693314182a9ebbd1dc3b59e0df9955
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-17 23:54:43 +0530

    some changes in nav bar

--- Project/public/app/social-common.js
+++ Project/public/app/social-common.js
@@ -1,10 +1,26 @@
 $(()=>{
     $('#navbar').load('./components/navbar.html',loginIfNeeded) //callback function is available at load().
+    //first loading of component done then only loginIfNeeded will execute.
     $('#footer').load('./components/footer.html')
-    
 
-})
 function loginIfNeeded(){
+  let currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null;
+  if (!currentUser) {
+    $.post('/api/users', {}, (user) => {
+      if (user) {
+        console.log('registered current user as ', user.username)
+        window.localStorage.user = JSON.stringify(user)
+        currentUser = user
+        $('#nav-username').text(currentUser.username)
+      }
+    })
+  } else {
+    console.log('resuming session as ', currentUser.username)
+    console.log($('#nav-username'))
+    $('#nav-username').text(currentUser.username)
+  }
+}
+/*
     let currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null;
     console.log(currentUser)
     if(!currentUser){
@@ -20,6 +36,9 @@ function loginIfNeeded(){
     } else {
         console.log('resuming session as '+ currentUser.username)
         console.log('nav-username')
-        $('#nav-username').text(currentUser.username)
+        $('#nav-username').text("currentUser.username")
+        console.log($('#nav-username').val(currentUser.username))
     }
 }
+*/
+})
