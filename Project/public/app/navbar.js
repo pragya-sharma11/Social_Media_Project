6d65cbd5336ca34707d590d6ea0f961ad7ad0ab0
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-18 02:37:57 +0530

    make navigations using clicks

--- Project/public/app/navbar.js
+++ Project/public/app/navbar.js
@@ -1,5 +1,6 @@
 let navlinks = $('.navbar-nav .nav-link')
 
 navlinks.click((ev)=>{
-    console.log($(ev.target).attr('data-component'))
+  let componentUrl = `../components/${$(ev.target).attr('data-component')}`
+  $('#content').load(componentUrl)
 })
