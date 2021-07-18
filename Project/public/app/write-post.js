465316a4f062ca1311b5c6e39a8f2e361f14dec6
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-18 13:34:14 +0530

    some fixes

--- Project/public/app/write-post.js
+++ Project/public/app/write-post.js
@@ -3,10 +3,10 @@ $(()=>{
         const userId = JSON.parse(window.localStorage.user).id
         const title = $('#p-title').val()
         const body = $('#p-body').val()
-         $.post('/api/posts', (userId, title, body)=>{
+         $.post('/api/posts', {userId, title, body})
             $('#content').load('/components/all-posts.html')
             $('.nav-item active').removeClass('active')
-            $("[data-components = all-posts.html]").addClass('active')
-         })
+            $("[data-components = all-posts]").addClass('active')
+         
     })
 })
