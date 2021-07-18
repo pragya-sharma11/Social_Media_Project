e2fc491a92cb6f0558cd1b1eaf2f5731e7f71492
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-18 13:27:34 +0530

    load content of all-posts

--- Project/public/app/write-post.js
+++ Project/public/app/write-post.js
@@ -5,6 +5,8 @@ $(()=>{
         const body = $('#p-body').val()
          $.post('/api/posts', (userId, title, body)=>{
             $('#content').load('/components/all-posts.html')
+            $('.nav-item active').removeClass('active')
+            $("[data-components = all-posts.html]").addClass('active')
          })
     })
 })
