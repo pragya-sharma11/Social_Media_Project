f8d718fd65efdb8d117303e4341d018b21ec0e07
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-18 13:23:12 +0530

    get values of userid, title, body of post

--- Project/public/app/write-post.js
+++ Project/public/app/write-post.js
@@ -1,5 +1,8 @@
 $(()=>{
     $('#write-btn').click(()=>{
+        const userId = JSON.parse(window.localStorage.user).id
+        const title = $('#p-title').val()
+        const body = $('#p-body').val()
         
     })
 })
