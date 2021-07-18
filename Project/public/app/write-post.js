9018d5e5a09eba844bdd4ccf7bd529af78b86b04
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-18 13:25:18 +0530

    post the content on posts api

--- Project/public/app/write-post.js
+++ Project/public/app/write-post.js
@@ -3,6 +3,8 @@ $(()=>{
         const userId = JSON.parse(window.localStorage.user).id
         const title = $('#p-title').val()
         const body = $('#p-body').val()
-        
+         $.post('/api/posts', (userId, title, body)=>{
+            $('#content').load('/components/all-posts.html')
+         })
     })
 })
